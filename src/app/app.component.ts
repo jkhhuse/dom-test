65e61586a5b407874093a91fe880c518035d6745
jkhhuse <jkhhuse@126.com>
2018-04-18 17:37:11 +0800

    format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,33 +8,35 @@ import { Demo10Component } from './demo10/demo10.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
   title = 'app works!';
   component: ComponentRef<Demo10Component>;
-  
+
   constructor(
       private componentFactoryResolver: ComponentFactoryResolver,
       private elementRef: ElementRef,
       private injector: Injector,
-      private appRef: ApplicationRef
-  ) {
+      private appRef: ApplicationRef) {
       this.component = this.componentFactoryResolver
           .resolveComponentFactory(Demo10Component)
           .create(this.injector);
+      // changeDetectorRef.detectChanges()方法只会在组件状态改变后进行一次变化检测
+      // this.component.changeDetectorRef.detectChanges();
       appRef.attachView(this.component.hostView);
       (<Demo10Component>this.component.instance).onTitleChange
           .subscribe(() => {
-              console.log("title clicked")
+              console.log('title clicked');
           });
-      (<Demo10Component>this.component.instance).title = "a works again!";
+      (<Demo10Component>this.component.instance).title = 'a works again!';
+
   }
 
   ngAfterViewInit() {
-      let host = document.createElement("div");
+      const host = document.createElement('div');
       host.appendChild((this.component.hostView as any).rootNodes[0]);
       this.elementRef.nativeElement.appendChild(host);
   }
-  
+
   ngOnDestroy() {
       this.appRef.detachView(this.component.hostView);
       this.component.destroy();
